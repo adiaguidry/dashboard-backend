d9d617f37520db749dbe4096d06b8465d6c1c620
adiaguidry <adiaguidry@gmail.com>
2020-01-20 22:24:10 -0700

    added support for reading connection string from an env var

--- app.js
+++ app.js
@@ -14,122 +14,124 @@ app.use(express.json());
 //dash_db = mongodb+srv://JaneDoe:<password>@adiascloud-stqqj.mongodb.net/test?retryWrites=true&w=majority
 
 mongoose
-  .connect("dash_db")
+  .connect(
+    "mongodb+srv://JaneDoe:1234@adiascloud-stqqj.mongodb.net/test?retryWrites=true&w=majority"
+  )
   .then(() => console.log("Connected to MongoDB...You got it Adia"))
   .catch(err => console.error("could not connect", err));
 
 //create schema
-const healthSchema = new mongoose.Schema({
-  name: String,
-  calorieTarget: Number,
-  week: [
-    {
-      name: String,
-      if: Number,
-      workout: Number,
-      calories: Number,
-      calPercent: Number,
-      fats: Number,
-      protien: Number,
-      carbs: Number,
-      sleep: Number
-    }
-  ]
-});
+// const healthSchema = new mongoose.Schema({
+//   name: String,
+//   calorieTarget: Number,
+//   week: [
+//     {
+//       name: String,
+//       if: Number,
+//       workout: Number,
+//       calories: Number,
+//       calPercent: Number,
+//       fats: Number,
+//       protien: Number,
+//       carbs: Number,
+//       sleep: Number
+//     }
+//   ]
+// });
 
-//set class
-const HealthTrack = mongoose.model("HealthTrack", healthSchema);
+// //set class
+// const HealthTrack = mongoose.model("HealthTrack", healthSchema);
 
-//create goal
-async function createHealthTrack() {
-  //create an object
-  const healthTrack = new HealthTrack({
-    name: "Adia",
-    calorieTarget: 2000,
-    week: [
-      {
-        name: "Sun",
-        if: 16,
-        workout: 40,
-        calories: 1919,
-        calPercent: 90,
-        fats: 133,
-        protien: 132,
-        carbs: 18,
-        sleep: 4
-      },
-      {
-        name: "Mon",
-        if: 18,
-        workout: 30,
-        calories: 1789,
-        calPercent: 80,
-        fats: 157,
-        protien: 132,
-        carbs: 48,
-        sleep: 8
-      },
-      {
-        name: "Tues",
-        if: 10,
-        workout: 30,
-        calories: 2119,
-        calPercent: 100,
-        fats: 163,
-        protien: 112,
-        carbs: 38,
-        sleep: 6
-      },
-      {
-        name: "Wed",
-        if: 16,
-        workout: 15,
-        calories: 1519,
-        calPercent: 75,
-        fats: 153,
-        protien: 152,
-        carbs: 22,
-        sleep: 8
-      },
-      {
-        name: "Thur",
-        if: 12,
-        workout: 15,
-        calories: 2000,
-        calPercent: 100,
-        fats: 133,
-        protien: 102,
-        carbs: 20,
-        sleep: 7
-      },
-      {
-        name: "Fri",
-        if: 13,
-        workout: 10,
-        calories: 2219,
-        calPercent: 100,
-        fats: 143,
-        protien: 172,
-        carbs: 55,
-        sleep: 5
-      },
-      {
-        name: "Sat",
-        if: 14,
-        workout: 10,
-        calories: 1902,
-        calPercent: 95,
-        fats: 163,
-        protien: 123,
-        carbs: 21,
-        sleep: 4
-      }
-    ]
-  });
+// //create goal
+// async function createHealthTrack() {
+//   //create an object
+//   const healthTrack = new HealthTrack({
+//     name: "Jane Doe",
+//     calorieTarget: 2000,
+//     week: [
+//       {
+//         name: "Sun",
+//         if: 16,
+//         workout: 40,
+//         calories: 1919,
+//         calPercent: 90,
+//         fats: 133,
+//         protien: 132,
+//         carbs: 18,
+//         sleep: 4
+//       },
+//       {
+//         name: "Mon",
+//         if: 18,
+//         workout: 30,
+//         calories: 1789,
+//         calPercent: 80,
+//         fats: 157,
+//         protien: 132,
+//         carbs: 48,
+//         sleep: 8
+//       },
+//       {
+//         name: "Tues",
+//         if: 10,
+//         workout: 30,
+//         calories: 2119,
+//         calPercent: 100,
+//         fats: 163,
+//         protien: 112,
+//         carbs: 38,
+//         sleep: 6
+//       },
+//       {
+//         name: "Wed",
+//         if: 16,
+//         workout: 15,
+//         calories: 1519,
+//         calPercent: 75,
+//         fats: 153,
+//         protien: 152,
+//         carbs: 22,
+//         sleep: 8
+//       },
+//       {
+//         name: "Thur",
+//         if: 12,
+//         workout: 15,
+//         calories: 2000,
+//         calPercent: 100,
+//         fats: 133,
+//         protien: 102,
+//         carbs: 20,
+//         sleep: 7
+//       },
+//       {
+//         name: "Fri",
+//         if: 13,
+//         workout: 10,
+//         calories: 2219,
+//         calPercent: 100,
+//         fats: 143,
+//         protien: 172,
+//         carbs: 55,
+//         sleep: 5
+//       },
+//       {
+//         name: "Sat",
+//         if: 14,
+//         workout: 10,
+//         calories: 1902,
+//         calPercent: 95,
+//         fats: 163,
+//         protien: 123,
+//         carbs: 21,
+//         sleep: 4
+//       }
+//     ]
+//   });
 
-  const result = await healthTrack.save();
-  console.log(result);
-}
+//   const result = await healthTrack.save();
+//   console.log(result);
+// }
 
 app.get("/healthtracker", async (req, res) => {
   const healthtracker = await HealthTrack.find();
